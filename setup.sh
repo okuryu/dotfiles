047a8834f8ece85270d9877cacc1281386e8b373
Ryuichi Okumura <okuryu@okuryu.com>
2012-03-25 00:54:48 +0900

    Improve setup dot files

--- setup.sh
+++ setup.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 LOCAL_DIR=`pwd`
+DOT_FILES="
+.bash_profile
+.tmux.conf
+.vimrc
+.zshrc
+"
 VIM=.vim
-VIMRC=.vimrc
-TMUXCONF=.tmux.conf
 ZSH=.zsh
-ZSHRC=.zshrc
-BASH_PROFILE=.bash_profile
 LOCAL_VIM=$LOCAL_DIR/$VIM
-LOCAL_VIMRC=$LOCAL_DIR/$VIMRC
-LOCAL_TMUXCONF=$LOCAL_DIR/$TMUXCONF
-LOCAL_ZSHRC=$LOCAL_DIR/$ZSHRC
-LOCAL_BASH_PROFILE=$LOCAL_DIR/$BASH_PROFILE
 HOME_VIM=$HOME/$VIM
-HOME_VIMRC=$HOME/$VIMRC
-HOME_TMUXCONF=$HOME/$TMUXCONF
 HOME_ZSH=$HOME/$ZSH
-HOME_ZSHRC=$HOME/$ZSHRC
-HOME_BASH_PROFILE=$HOME/$BASH_PROFILE
 VIM_BUNDLE=$HOME_VIM/bundle
 VIM_AUTOLOAD=$HOME_VIM/autoload
 ZSH_COMPLETION=$HOME_ZSH/functions/Completion
+
+setup_dot_files () {
+  for DOT_FILE in $DOT_FILES
+  do
+    cp $LOCAL_DIR/$DOT_FILE $HOME/$DOT_FILE
+  done
+}
 
 main () {
   if [ ! -x `which git` ]
@@ -38,27 +39,12 @@ main () {
     rm -rf $HOME_VIM
   fi
   setup_vim
-  if [ -f $HOME_VIMRC ]
-  then
-    rm -rf $HOME_VIMRC
-  fi
-  setup_vimrc
-  setup_tmuxconf
   if [ -d $HOME_ZSH ]
   then
     rm -rf $HOME_ZSH
   fi
   setup_zsh
-  if [ -f $HOME_ZSHRC ]
-  then
-    rm -rf $HOME_ZSHRC
-  fi
-  setup_zshrc
-  if [ -f $HOME_BASH_PROFILE ]
-  then
-    rm -rf $HOME_BASH_PROFILE
-  fi
-  setup_bash_profile
+  setup_dot_files
 }
 
 setup_vim () {
@@ -74,27 +60,10 @@ setup_vim () {
   ln -s $HOME_VIM/bundle/vim-pathogen/autoload/pathogen.vim $VIM_AUTOLOAD
 }
 
-setup_vimrc () {
-  cp $LOCAL_VIMRC $HOME_VIMRC
-}
-
-setup_tmuxconf () {
-  cp $LOCAL_TMUXCONF $HOME_TMUXCONF
-}
-
 setup_zsh () {
   mkdir -p $ZSH_COMPLETION
   cd $ZSH_COMPLETION
   curl -k -O https://raw.github.com/knu/zsh-git-escape-magic/master/git-escape-magic
 }
 
-setup_zshrc () {
-  cp $LOCAL_ZSHRC $HOME_ZSHRC
-}
-
-setup_bash_profile () {
-  cp $LOCAL_BASH_PROFILE $HOME_BASH_PROFILE
-}
-
 main
-
